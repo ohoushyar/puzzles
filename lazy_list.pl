567dcb6e49d858aa7ddfad7cb54dc92ebab335ac
Omid Houshyar <ohoushyar@gmail.com>
2013-01-16 07:51:39 +0800

    added factorial list

--- lazy_list.pl
+++ lazy_list.pl
@@ -23,8 +23,19 @@ sub integers {
     );
 }
 
+sub factorials {
+    my $n = shift;
+    return LazyList->new(
+        value => ($n <= 1) ? 1 : $n * factorials($n-1)->value,
+        rest => sub { factorials($n+1) },
+    );
+}
+
 my $list = integers(101);
 is $list->nth(10), 110, 'got the value of 10th element from initial list';
+
+my $flist = factorials(5);
+is $flist->nth(3), 5040, 'got the factorial of 5th elements';
 
 
 done_testing();
